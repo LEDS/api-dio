93eaa748e9cebaf79c85f175c4c070420ff741c2
icaro <icarodgl@gmail.com>
2018-06-22 16:23:05 -0300

    chore(latest): criado controller e classe

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsService } from './news.service';
 import { NewsController } from './news/news.controller';
+import { LatestController } from './latest/latest.controller';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, NewsController],
+  controllers: [AppController, NewsController, LatestController],
   providers: [AppService, NewsService],
 })
 export class AppModule {}
